4c0e0bb43345a25e89877dd825e71741496e01df
Oksana Furman <oksan9a.furman@gmail.com>
2023-03-27 11:18:22 +0200

    create folders and files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "components/boot.php"; ?>
+    <title>PHP Day 5</title>
+</head>
+<body>
+    
+</body>
+</html>
