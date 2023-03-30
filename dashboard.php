75dc73aa39059b4bdd71e68d4b7693934a9531d2
oksana-furman <122574885+oksana-furman@users.noreply.github.com>
2023-03-30 14:53:40 +0200

    update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -8,9 +8,32 @@
     } 
     require "./components/db-connect.php";
 
-    $sql = "SELECT * FROM users WHERE id = {$_SESSION['adm']}";
+    $id = $_SESSION['adm'];
+    $status = 'adm';
+    $sql = "SELECT * FROM users WHERE status != '$status'";
     $result = mysqli_query($connect, $sql);
-    $row = mysqli_fetch_assoc($result);
+
+    $sql2 = "SELECT * FROM users WHERE id = {$_SESSION['adm']}";
+    $result2 = mysqli_query($connect, $sql2);
+    $row2 = mysqli_fetch_assoc($result2);
+
+    $tbody = '';
+    if (mysqli_num_rows($result) > 1) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tbody .= "
+        <tr>
+          <td><img class='img-thumbnail rounded-circle' src='./pictures/{$row['picture']}' alt='{$row['first_name']}'></td>
+          <td>{$row['first_name']} {$row['last_name']}</td>
+          <td>{$row['date_of_birth']}</td>
+          <td>{$row['email']}</td>
+          <td><a href='update_profile.php?id={$row['id']}'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
+          <a href='delete_profile.php?id={$row['id']}'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
+       </tr>";  
+        } 
+    } else{
+        $tbody = "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
+    }
+    mysqli_close($connect);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,18 +43,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once "components/boot.php"; ?>
     <?php linkFun(0);  ?>
-    <title>Welcome <?= $row['first_name'] ?></title>
+    <title>Welcome <?= $row2['first_name'] ?></title>
 </head>
 <body>
     <?php require "./navbar.php"; ?>
     <div class="container">
         <div class="hero m-2">
             <p class="text-white">Administrator</p>
-            <img class="userImage m-2" src="pictures/<?php echo $row['picture']; ?>" alt="<?php echo $row['first_name']; ?>">
-            <p class="text-white">Hi <?php echo $row['first_name']; ?></p>
+            <img class="userImage m-2" src="pictures/<?= $row2['picture']; ?>" alt="<?= $row2['first_name']; ?>">
+            <p class="text-white">Hi <?= $row2['first_name']; ?></p>
+            <div class="btnBox">
+                 <a class="link m-2" href="logout.php?logout">Sign Out</a>
+        <a class="link m-2" href="update_profile.php?id=<?= $_SESSION['adm'] ?>">Update your profile</a>
+            </div>
+
+            <div class="col-8 offset-2 mt-3 mb-3">
+              <p class='h2'>Users</p>
+              <table class='table'>
+                  <thead class='table-success'>
+                      <tr>
+                          <th>Picture</th>
+                          <th>Name</th>
+                          <th>Date of birth</th>
+                          <th>Email</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>
+                  <tbody class="text-white">
+                      <?= $tbody ?>
+                  </tbody>
+              </table>
+          </div>
         </div>
-        <a class="link m-2" href="logout.php?logout">Sign Out</a>
-        <a class="link m-2" href="update.php?id=<?php echo $_SESSION['adm'] ?>">Update your profile</a>
+       
+
     </div>
 
     <?php require "./cars/footer.php"; ?>
